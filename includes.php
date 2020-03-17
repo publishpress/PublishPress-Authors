cd807124f8fb38b73cd4bf6a11c4a8c1f9345d1a
Anderson Grudtner Martins <anderson@publishpress.com>
2020-03-17 14:28:57 -0300

    Bump version to 3.2.4-alpha.1

--- includes.php
+++ includes.php
@@ -12,7 +12,7 @@
 defined('ABSPATH') or die('No direct script access allowed.');
 
 if ( ! defined('PP_AUTHORS_LOADED')) {
-    define('PP_AUTHORS_VERSION', '3.2.3');
+    define('PP_AUTHORS_VERSION', '3.2.4-alpha.1');
     define('PP_AUTHORS_FILE', 'publishpress-authors/publishpress-authors.php');
     define('PP_AUTHORS_BASE_PATH', plugin_dir_path(__FILE__));
     define('PP_AUTHORS_MODULES_PATH', PP_AUTHORS_BASE_PATH . 'modules');
