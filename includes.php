2d9332728ecc8d9f7a6b72e2e928c8e58bd39749
Anderson Grudtner Martins <anderson@publishpress.com>
2020-03-17 14:28:27 -0300

    Add compatibility with composer based sites

--- includes.php
+++ includes.php
@@ -30,9 +30,13 @@ if ( ! defined('PP_AUTHORS_LOADED')) {
     require_once __DIR__ . '/template-tags.php';
     require_once __DIR__ . '/integrations/amp.php';
 
-    if (is_admin()) {
-        require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'publishpress' . DIRECTORY_SEPARATOR
-                     . 'wordpress-version-notices' . DIRECTORY_SEPARATOR . 'includes.php';
+    if (is_admin() && !defined('PUBLISHPRESS_AUTHORS_SKIP_VERSION_NOTICES')) {
+        $includesPath = __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'publishpress' . DIRECTORY_SEPARATOR
+                        . 'wordpress-version-notices' . DIRECTORY_SEPARATOR . 'includes.php';
+
+        if (file_exists($includesPath)) {
+            require_once $includesPath;
+        }
 
         add_filter(\PPVersionNotices\Module\TopNotice\Module::SETTINGS_FILTER, function ($settings) {
             $settings['publishpress-authors'] = [
