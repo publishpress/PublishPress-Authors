33b573f08c8478e5312041b57b90dfa16255a257
Anderson Grudtner Martins <anderson@publishpress.com>
2020-03-27 18:06:34 -0300

    Bump version to 3.2.4-beta.1

--- publishpress-authors.php
+++ publishpress-authors.php
@@ -12,7 +12,7 @@
  * @publishpress-authors
  * Plugin Name: PublishPress Authors
  * Plugin URI:  https://publishpress.com/
- * Version: 3.2.4-alpha.2
+ * Version: 3.2.4-beta.1
  * Description: Add support for multiple authors
  * Author:      PublishPress
  * Author URI:  https://publishpress.com
