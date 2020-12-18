192a01bdb81e17a44c984edaf1671e5cff18a293
Anderson Grudtner Martins <anderson@publishpress.com>
2020-12-17 22:47:36 -0300

    Returns Author instance if post author has a term, fix #306

--- src/functions/template-tags.php
+++ src/functions/template-tags.php
@@ -126,17 +126,26 @@ if (!function_exists('get_multiple_authors')) {
             } else {
                 // Fallback to the post author
                 $post = get_post($postId);
-                $user = get_user_by('id', $post->post_author);
 
-                if ($user) {
-                    $user->link = get_author_posts_url($user->ID);
+                $author = Author::get_by_user_id($post->post_author);
 
-                    if ($filter_the_author) {
-                        $user->display_name = apply_filters('the_author', $user->display_name);
+                if (empty($author) || is_wp_error($author)) {
+                    $user = get_user_by('id', $post->post_author);
+
+                    if ($user) {
+                        $user->link = get_author_posts_url($user->ID);
+
+                        if ($filter_the_author) {
+                            $user->display_name = apply_filters('the_author', $user->display_name);
+                        }
+
+                        $author = $user;
+                    } else {
+                        return [];
                     }
-
-                    $authors = [$user];
                 }
+
+                $authors[] = $author;
             }
 
             wp_cache_set($cacheKey, $authors, 'get_multiple_authors:authors');
