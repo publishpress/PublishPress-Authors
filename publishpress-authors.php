13799238a9664cb5038300743d0827d962635e3e
Anderson Grudtner Martins <anderson@publishpress.com>
2021-02-05 16:20:57 -0300

    Update version number

--- publishpress-authors.php
+++ publishpress-authors.php
@@ -5,7 +5,7 @@
  * Description: PublishPress Authors allows you to add multiple authors and guest authors to WordPress posts
  * Author:      PublishPress
  * Author URI:  https://publishpress.com
- * Version: 3.11.0
+ * Version: 3.11.0-hotfix-345-nested-pages-php-warnings
  * Text Domain: publishpress-authors
  *
  * ------------------------------------------------------------------------------
