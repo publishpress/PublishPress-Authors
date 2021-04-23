05b085d102a5bf56a198363ac83b13e46355c273
Anderson Grudtner Martins <anderson@publishpress.com>
2021-04-23 16:31:56 -0300

    Update version number

--- publishpress-authors.php
+++ publishpress-authors.php
@@ -5,7 +5,7 @@
  * Description: PublishPress Authors allows you to add multiple authors and guest authors to WordPress posts
  * Author:      PublishPress
  * Author URI:  https://publishpress.com
- * Version: 3.13.1
+ * Version: 3.13.1-hotfix-18-polylang
  * Text Domain: publishpress-authors
  *
  * ------------------------------------------------------------------------------
