8993e6eb93cd66bf01c095478286e87a9aca646e
andergmartins <andergmartins@gmail.com>
2021-11-09 21:04:14 -0300

    Bump version to v3.14.9-beta.1

--- publishpress-authors.php
+++ publishpress-authors.php
@@ -5,7 +5,7 @@
  * Description: PublishPress Authors allows you to add multiple authors and guest authors to WordPress posts
  * Author:      PublishPress
  * Author URI:  https://publishpress.com
- * Version: 3.14.8
+ * Version: 3.14.9-beta.1
  * Text Domain: publishpress-authors
  *
  * ------------------------------------------------------------------------------
