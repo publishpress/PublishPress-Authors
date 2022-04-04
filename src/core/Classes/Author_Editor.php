4b6d4c0b4a832a503d48931778c65ac4ac6c1036
ojopaul <ojopaul.dev@gmail.com>
2022-04-04 21:39:15 +0100

    - Format code to be PR-12 compliance

--- src/core/Classes/Author_Editor.php
+++ src/core/Classes/Author_Editor.php
@@ -822,13 +822,11 @@ class Author_Editor
             }
 
             $author_slug_user = get_user_by('slug', $slug);
-            if (
-                $author_slug_user
+            if ($author_slug_user
                 && is_object($author_slug_user)
                 && isset($author_slug_user->ID)
             ) {
-                if (
-                    (! isset($_POST['authors-new']))
+                if ((! isset($_POST['authors-new']))
                     || ((int)$author_slug_user->ID != (int)$_POST['authors-new'])
                 ) {
                     return new WP_Error(
