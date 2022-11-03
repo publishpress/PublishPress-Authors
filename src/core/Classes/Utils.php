a81d3ef2ae4bfd3b2acb7f7abbc5272d724e3f5e
Ojo Paul <ojopaul.dev@gmail.com>
2022-11-03 11:49:10 +0100

    - Fix default layout issue

--- src/core/Classes/Utils.php
+++ src/core/Classes/Utils.php
@@ -747,6 +747,12 @@ class Utils
             'posts_per_page' => 1
         ];
         $default_post = get_posts($args);
+        if (empty($default_post)) {
+            //recreate default
+            MA_Author_Boxes::createDefaultAuthorBoxes();
+            $default_post = get_posts($args);
+        }
+        
         if (!empty($default_post)) {
             $default_layout = MA_Author_Boxes::POST_TYPE_BOXES . '_' . $default_post[0]->ID;
         } else {
