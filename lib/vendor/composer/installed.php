f574e19a98a4d2976d78964509e65c66312e0c49
ojopaul <ojopaul.dev@gmail.com>
2023-12-19 23:03:13 +0100

    composer update

--- lib/vendor/composer/installed.php
+++ lib/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'c09aa99f59eb3e1494d9af2c80d981f1b2c7d587',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'c09aa99f59eb3e1494d9af2c80d981f1b2c7d587',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
