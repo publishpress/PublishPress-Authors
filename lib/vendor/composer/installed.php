16262c08fc7e45eedbf31c1872036b0a0c3ae6c7
Ojo Paul <ojopaul.dev@gmail.com>
2024-02-22 09:43:36 +0100

    Composer update

--- lib/vendor/composer/installed.php
+++ lib/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '2ea57ddb1ddc3fc7d0216a7b19d83beb426056a1',
+        'reference' => '572b2c80e8f4399421f1246e9bcd9fbda15025c6',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '2ea57ddb1ddc3fc7d0216a7b19d83beb426056a1',
+            'reference' => '572b2c80e8f4399421f1246e9bcd9fbda15025c6',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
