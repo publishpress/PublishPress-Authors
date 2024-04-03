8892bf0588298240210afcc777a0f8614c1d163d
Ojo Paul <ojopaul.dev@gmail.com>
2024-04-03 13:55:33 +0100

    Composer update

--- lib/vendor/composer/installed.php
+++ lib/vendor/composer/installed.php
@@ -20,9 +20,9 @@
             'dev_requirement' => false,
         ),
         'cmb2/cmb2' => array(
-            'pretty_version' => 'v2.10.1',
-            'version' => '2.10.1.0',
-            'reference' => '4afc4bb7b92ab6d93aac2247c9a84af773e42532',
+            'pretty_version' => 'v2.11.0',
+            'version' => '2.11.0.0',
+            'reference' => '2847828b5cce1b48d09427ee13e6f7c752704468',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../cmb2/cmb2',
             'aliases' => array(),
