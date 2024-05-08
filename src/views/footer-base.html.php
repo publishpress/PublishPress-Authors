a99f9d29fc873149ea2af6955e0a2150aabde369
Ojo Paul <ojopaul.dev@gmail.com>
2024-05-08 14:01:10 +0100

    Fix text domain

--- src/views/footer-base.html.php
+++ src/views/footer-base.html.php
@@ -11,23 +11,23 @@ $rating_star .= '<span class="dashicons dashicons-star-filled"></span>';
             href="//wordpress.org/support/plugin/<?php echo esc_attr($context['plugin_slug']); ?>/reviews/#new-post"
             target="_blank"
             rel="noopener noreferrer">
-            <?php printf(esc_html__('If you like %1$s please leave us a %2$s rating. Thank you!', ' publishpress-authors'), '<strong>'. $context['plugin_name'] .'</strong>', $rating_star); ?>
+            <?php printf(esc_html__('If you like %1$s please leave us a %2$s rating. Thank you!', 'publishpress-authors'), '<strong>'. $context['plugin_name'] .'</strong>', $rating_star); ?>
         </a>
     </div>
     <hr>
     <nav>
         <ul>
             <li>
-                <a href="//publishpress.com/authors/" target="_blank" rel="noopener noreferrer" title="About <?php echo esc_attr($context['plugin_name']); ?>"><?php esc_html_e('About', ' publishpress-authors'); ?></a>
+                <a href="//publishpress.com/authors/" target="_blank" rel="noopener noreferrer" title="About <?php echo esc_attr($context['plugin_name']); ?>"><?php esc_html_e('About', 'publishpress-authors'); ?></a>
             </li>
             <li>
                 <a href="//publishpress.com/knowledge-base/authors-getting-started/" target="_blank"
                    rel="noopener noreferrer"
-                   title="Documentation"><?php esc_html_e('Documentation', ' publishpress-authors'); ?></a>
+                   title="Documentation"><?php esc_html_e('Documentation', 'publishpress-authors'); ?></a>
             </li>
             <li>
                 <a href="//publishpress.com/publishpress-support/" target="_blank" rel="noopener noreferrer"
-                   title="Contact the PublishPress team"><?php esc_html_e('Contact', ' publishpress-authors'); ?></a>
+                   title="Contact the PublishPress team"><?php esc_html_e('Contact', 'publishpress-authors'); ?></a>
             </li>
         </ul>
     </nav>
