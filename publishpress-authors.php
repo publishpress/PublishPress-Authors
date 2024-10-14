e0ef06ea03958e47e03658e3e9cfb546bc495a85
Ojo Paul <ojopaul.dev@gmail.com>
2024-10-14 10:34:06 +0100

    Update version

--- publishpress-authors.php
+++ publishpress-authors.php
@@ -5,7 +5,7 @@
  * Description: PublishPress Authors allows you to add multiple authors and guest authors to WordPress posts
  * Author:      PublishPress
  * Author URI:  https://publishpress.com
- * Version: 4.7.1
+ * Version: 4.7.2
  * Text Domain: publishpress-authors
  * Domain Path: /languages
  * Requires at least: 5.5
