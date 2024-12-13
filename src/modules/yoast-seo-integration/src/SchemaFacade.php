209d1c69c2d03d7cd377caace3cadd690058c48e
Ojo Paul <ojopaul.dev@gmail.com>
2024-12-13 11:43:05 +0100

    Missing Schema on Page #1926

--- src/modules/yoast-seo-integration/src/SchemaFacade.php
+++ src/modules/yoast-seo-integration/src/SchemaFacade.php
@@ -189,9 +189,7 @@ class SchemaFacade
             if (!is_array($piece)) {
                 continue;
             }
-            if (isset($piece['author'])) {
-                $data[$key]['author'] = $authors;
-            }
+            
             if (count($author_objects) === 1 && isset($piece['@type']) && !is_array($piece['@type']) && $piece['@type'] === 'Person') {
                 $data[$key] = wp_parse_args($author_data, $piece);
             }
